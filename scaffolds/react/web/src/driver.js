d0df409c2ef8486a5821eab5419342f19216eb8a
Fred Snyder <fsnyder@castwide.com>
2019-04-10 19:43:16 -0400

    Web app exports an Opal interface.

--- scaffolds/react/web/src/driver.js
+++ scaffolds/react/web/src/driver.js
@@ -1,5 +1,29 @@
-import './engine.rb';
-import {OpalDriver} from 'gamefic-driver';
+import './opal.rb';
+import { OpalDriver } from 'gamefic-driver';
 
-const driver = new OpalDriver(Opal);
-export {driver};
+let start = function() {
+    var state = Opal.gvars.character.$state();
+    var response = JSON.parse(state.$to_json());
+    return response;
+};
+
+let receive = function (input) {
+    Opal.gvars.character.$queue().$push(input);
+};
+
+let update = function () {
+    Opal.gvars.plot.$update();
+    Opal.gvars.plot.$ready();
+    var state = Opal.gvars.character.$state();
+    var response = JSON.parse(state.$to_json());
+    return response;
+};
+
+const engine = {
+    start: start,
+    receive: receive,
+    update: update
+};
+  
+const driver = new OpalDriver(engine);
+export { driver };
