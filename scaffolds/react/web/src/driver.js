6c06ba5ecf60b17af38b8c9e2de9f40a69534c7c
Fred Snyder <fsnyder@castwide.com>
2019-04-10 23:40:57 -0400

    Reverted back to Opal reference in web app driver.

--- scaffolds/react/web/src/driver.js
+++ scaffolds/react/web/src/driver.js
@@ -1,29 +1,5 @@
 import './opal.rb';
 import { OpalDriver } from 'gamefic-driver';
-
-let start = function() {
-    var state = Opal.gvars.character.$state();
-    var response = JSON.parse(state.$to_json());
-    return response;
-};
-
-let receive = function (input) {
-    Opal.gvars.character.$queue().$push(input);
-};
-
-let update = function () {
-    Opal.gvars.plot.$update();
-    Opal.gvars.plot.$ready();
-    var state = Opal.gvars.character.$state();
-    var response = JSON.parse(state.$to_json());
-    return response;
-};
-
-const engine = {
-    start: start,
-    receive: receive,
-    update: update
-};
   
-const driver = new OpalDriver(engine);
+const driver = new OpalDriver(Opal);
 export { driver };
