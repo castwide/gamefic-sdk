46fc54db1387c090f5c7919d0b8fff62464265c8
Fred Snyder <fsnyder@castwide.com>
2019-04-14 10:47:29 -0400

    React app uses Terminal.

--- scaffolds/react/web/src/index.js
+++ scaffolds/react/web/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Console} from 'react-gamefic';
-import {driver} from './driver';
+import { render } from 'react-dom';
+import { Console, Terminal } from 'react-gamefic';
+import { driver } from './driver';
 import 'react-gamefic/styles/ebook';
 import './style.css';
 
 render(
-	<Console
-		driver={driver}
-		autoScroll={true}
-	/>,
+	<Console driver={driver}>
+		<Terminal />
+	</Console>,
 	document.getElementById('root')
 );
