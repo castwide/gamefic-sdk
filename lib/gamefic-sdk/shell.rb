bb0405453a0af6f2c529f20550102bc071bcd278
Fred Snyder <fsnyder@castwide.com>
2024-10-29 19:30:21 -0400

    Remove stale diagram subcommand

--- lib/gamefic-sdk/shell.rb
+++ lib/gamefic-sdk/shell.rb
@@ -25,23 +25,6 @@ module Gamefic
         Gamefic::Sdk::Scaffold.build 'library', directory_name
         puts "Gamefic library initialized at #{FIle.realpath(directory_name)}"
       end
-
-      desc 'diagram TYPE', 'Get diagram data'
-      long_desc %(
-        SDK "diagrams" are datasets that can be used in analysis tools and
-        graphical data representations. The dataset is provided in JSON
-        format.
-
-        The diagram types are entities, rooms, actions, verbs, syntaxes, and commands.
-      )
-      option :directory, type: :string, aliases: :d, desc: 'The project directory', default: '.'
-      def diagram type
-        main = Pathname.new(options[:directory]).join('main.rb').realpath.to_s
-        require_relative main
-        plot = Gamefic::Plot.new
-        diagram = Gamefic::Sdk::Diagram.new(plot)
-        puts diagram.get(type).to_json
-      end
     end
   end
 end
